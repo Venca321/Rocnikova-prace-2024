7e3cd18a6d8b60d7a47f545e95d2d2f2d827e6c6
venca321 <login@vaclavparma.cz>
2023-09-13 08:38:03 +0200

    Move code to HandRecognition.py

---
+++ engine/HandRecognition.py
@@ -0,0 +1,25 @@
+
+import mediapipe as mp
+import cv2
+
+class HandRecognition:
+    def __init__(self, mode=False, maxHands=1, modelComplex=1, detectionCon=0.5, trackCon=0.5): 
+        self.video = cv2.VideoCapture(0)
+        self.image = None
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(mode, maxHands, modelComplex, detectionCon, trackCon)
+
+    def processImage(self):
+        _, image = self.video.read()
+        imageRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        results = self.hands.process(imageRGB)
+        self.image = image
+        return results
+
+    def drawHandLandmarks(self, handLms):
+        mpDraw = mp.solutions.drawing_utils
+        mpDraw.draw_landmarks(self.image, handLms, self.mpHands.HAND_CONNECTIONS)
+
+    def stop(self):
+        self.video.release()
+        cv2.destroyAllWindows()
