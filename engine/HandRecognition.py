0120126040964ee7495d7d1619928e3044b9583a
venca321 <login@vaclavparma.cz>
2023-09-19 11:32:22 +0200

    Split screen for 2 players

--- engine/HandRecognition.py
+++ engine/HandRecognition.py
@@ -9,27 +9,36 @@ class HandRecognition:
     def __init__(self, mode=False, maxHands=1, modelComplex=1, detectionCon=0.5, trackCon=0.5): 
         self.video = cv2.VideoCapture(0)
         self.image = None
+        self.image1 = None
+        self.image2 = None
         self.mpHands = mp.solutions.hands
         self.hands = self.mpHands.Hands(mode, maxHands, modelComplex, detectionCon, trackCon)
 
-    def processImage(self) -> list:
+    def processImage(self) -> [list, list]:
         """
         Return list of hand maps
         """
         _, image = self.video.read()
-        imageRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        results = self.hands.process(imageRGB)
         self.image = image
+        self.image0 = image[0:image.shape[0], 0:int(image.shape[1]/2)]
+        self.image1 = image[0:image.shape[0], int(image.shape[1]/2):image.shape[1]]
+        
+        imageRGB0 = cv2.cvtColor(self.image0, cv2.COLOR_BGR2RGB)
+        results0 = self.hands.process(imageRGB0).multi_hand_landmarks
+        imageRGB1 = cv2.cvtColor(self.image1, cv2.COLOR_BGR2RGB)
+        results1 = self.hands.process(imageRGB1).multi_hand_landmarks
 
-        if results.multi_hand_landmarks: return results.multi_hand_landmarks
-        else: return []
 
-    def drawHandLandmarks(self, handMaps):
+        if not results0: results0 = []
+        if not results1: results1 = []
+        return [results0, results1]
+
+    def drawHandLandmarks(self, handMaps, image):
         """
         Draw hand landmarks on image
         """
         mpDraw = mp.solutions.drawing_utils
-        mpDraw.draw_landmarks(self.image, handMaps, self.mpHands.HAND_CONNECTIONS)
+        mpDraw.draw_landmarks(image, handMaps, self.mpHands.HAND_CONNECTIONS)
 
     def stop(self):
         self.video.release()
--- main.py
+++ main.py
@@ -4,56 +4,87 @@ from engine.HandProcessor import HandProcessor
 import cv2
 
 DEBUG_MODE = True
+LIMIT = 3
 
 Hand_recognition = HandRecognition()
 
-detected_gesture = 0
-last_gestures = []
+detected_gesture0 = 0
+detected_gesture1 = 0
+last_gestures0 = []
+last_gestures1 = []
 
 game_running = False
+player0_ready = False
 player1_ready = False
-player2_ready = True
 
 while True:
     results = Hand_recognition.processImage()
 
-    if results == []: detected_gesture = 0
-    for handMap in results:
-        hand_processor = HandProcessor(handMap, Hand_recognition.image)
+    if results[0] == []: detected_gesture0 = 0
+    if results[1] == []: detected_gesture1 = 0
+
+    for handMap in results[0]:
+        hand_processor = HandProcessor(handMap, Hand_recognition.image0)
         gesture = hand_processor.process()
 
-        if last_gestures.count(gesture) >= 5:
-            if gesture == 1: detected_gesture = 1
-            elif gesture == 2: detected_gesture = 2
-            elif gesture == 3: detected_gesture = 3
-            elif gesture == 4: detected_gesture = 4
-            else: detected_gesture = 0
+        if last_gestures0.count(gesture) >= LIMIT:
+            if gesture == 1: detected_gesture0 = 1
+            elif gesture == 2: detected_gesture0 = 2
+            elif gesture == 3: detected_gesture0 = 3
+            elif gesture == 4: detected_gesture0 = 4
+            else: detected_gesture0 = 0
 
-        last_gestures.append(gesture)
-        if len(last_gestures) > 5: last_gestures.pop(0)
+        last_gestures0.append(gesture)
+        if len(last_gestures0) > LIMIT: last_gestures0.pop(0)
         
-        if DEBUG_MODE: Hand_recognition.drawHandLandmarks(handMap)
+        if DEBUG_MODE: Hand_recognition.drawHandLandmarks(handMap, Hand_recognition.image0)
+
+    for handMap in results[1]:
+        hand_processor = HandProcessor(handMap, Hand_recognition.image1)
+        gesture = hand_processor.process()
+
+        if last_gestures1.count(gesture) >= LIMIT:
+            if gesture == 1: detected_gesture1 = 1
+            elif gesture == 2: detected_gesture1 = 2
+            elif gesture == 3: detected_gesture1 = 3
+            elif gesture == 4: detected_gesture1 = 4
+            else: detected_gesture1 = 0
+
+        last_gestures1.append(gesture)
+        if len(last_gestures1) > LIMIT: last_gestures1.pop(0)
+        
+        if DEBUG_MODE: Hand_recognition.drawHandLandmarks(handMap, Hand_recognition.image1)
+
+    if not player0_ready and not game_running:
+        if detected_gesture0 == 4: player0_ready = True
 
     if not player1_ready and not game_running:
-        if detected_gesture == 4: player1_ready = True
-        else: print("Like to run game")
+        if detected_gesture1 == 4: player1_ready = True
     
-    if player1_ready and player1_ready: 
+    if player0_ready and player1_ready: 
         game_running = True
+        player0_ready = False
         player1_ready = False
-        #player2_ready = False
 
     if game_running:
         print("Running...")
         game_running = False
 
-    if detected_gesture == 1: detected_gesture_text = "Kamen"
-    elif detected_gesture == 2: detected_gesture_text = "Papir"
-    elif detected_gesture == 3: detected_gesture_text = "Nuzky"
-    elif detected_gesture == 4: detected_gesture_text = "Like"
-    else: detected_gesture_text = "-----"
-
-    Hand_recognition.image = cv2.putText(Hand_recognition.image, detected_gesture_text, (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2, cv2.LINE_AA)
+    if detected_gesture0 == 1: detected_gesture_text0 = "Kamen"
+    elif detected_gesture0 == 2: detected_gesture_text0 = "Papir"
+    elif detected_gesture0 == 3: detected_gesture_text0 = "Nuzky"
+    elif detected_gesture0 == 4: detected_gesture_text0 = "Like"
+    else: detected_gesture_text0 = "-----"
+    Hand_recognition.image = cv2.putText(Hand_recognition.image, detected_gesture_text0, (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2, cv2.LINE_AA)
+    
+    if detected_gesture1 == 1: detected_gesture_text1 = "Kamen"
+    elif detected_gesture1 == 2: detected_gesture_text1 = "Papir"
+    elif detected_gesture1 == 3: detected_gesture_text1 = "Nuzky"
+    elif detected_gesture1 == 4: detected_gesture_text1 = "Like"
+    else: detected_gesture_text1 = "-----"
+    Hand_recognition.image = cv2.putText(Hand_recognition.image, detected_gesture_text1, (int(Hand_recognition.image.shape[1]/2)+10, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2, cv2.LINE_AA)
+    
+    cv2.line(Hand_recognition.image, (int(Hand_recognition.image.shape[1]/2), 0), (int(Hand_recognition.image.shape[1]/2), Hand_recognition.image.shape[0]), (0, 0, 255), 2)
 
     cv2.imshow("Output", Hand_recognition.image)
     if cv2.waitKey(1) == ord('q'): break
