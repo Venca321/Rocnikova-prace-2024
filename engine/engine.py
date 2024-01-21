4a529816aa9238b7814e2893b54545671fb4d930
Venca321 <login@vaclavparma.cz>
2024-01-21 12:29:46 +0100

    I hate this code

--- engine/engine.py
+++ engine/engine.py
@@ -5,16 +5,18 @@ from engine.database import Database, UserStatusEnums, User, Session
 class GameEngine:
     def process(db:Database, session:Session, user:User) -> (Session, int, User):
         if user.id == session.user1_id:
-            user_status = session.user1_status
-            if user_status == UserStatusEnums.WAITING:
+            try:
+                user_status = session.user1_status
+                opponent_status = session.user2_status
+                opponent = db.get_user(session.user2_id)
+            except Exception:
                 return session, user_status, User("None", "?????", 0)
-            opponent_status = session.user2_status
-            opponent = db.get_user(session.user2_id)
         else:
-            user_status = session.user2_status
-            if user_status == UserStatusEnums.WAITING:
+            try:
+                user_status = session.user2_status
+                opponent_status = session.user1_status
+                opponent = db.get_user(session.user1_id)
+            except Exception:
                 return session, user_status, User("None", "?????", 0)
-            opponent_status = session.user1_status
-            opponent = db.get_user(session.user1_id)
 
         return session, user_status, opponent
