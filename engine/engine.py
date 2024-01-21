4975a5ec167238d0bc9919fe02c2275486b05857
Venca321 <login@vaclavparma.cz>
2024-01-21 12:25:53 +0100

    Try to write it like human

--- engine/engine.py
+++ engine/engine.py
@@ -6,13 +6,15 @@ class GameEngine:
     def process(db:Database, session:Session, user:User) -> (Session, int, User):
         if user.id == session.user1_id:
             user_status = session.user1_status
-            if user_status == UserStatusEnums.WAITING: return session, user_status, User("None", "?????", 0)
+            if user_status == UserStatusEnums.WAITING:
+                return session, user_status, User("None", "?????", 0)
             opponent_status = session.user2_status
-            #opponent = db.get_user(session.user2_id)
+            opponent = db.get_user(session.user2_id)
         else:
             user_status = session.user2_status
-            if user_status == UserStatusEnums.WAITING: return session, user_status, User("None", "?????", 0)
+            if user_status == UserStatusEnums.WAITING:
+                return session, user_status, User("None", "?????", 0)
             opponent_status = session.user1_status
-            #opponent = db.get_user(session.user1_id)
+            opponent = db.get_user(session.user1_id)
 
-        return session, user_status, User("None", "?????", 0)
+        return session, user_status, opponent
