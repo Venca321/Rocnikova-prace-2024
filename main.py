d7c81775ef5929e9fadc9399520c940ccc038517
Venca321 <login@vaclavparma.cz>
2024-01-21 10:51:06 +0100

    Debug connection

--- main.py
+++ main.py
@@ -98,20 +98,20 @@ def handle_image(data):
         gesture = GestureEnums.NONE
     
     db.update_user(user, gesture)
-    
+
     #Engine code
 
-    session = db.get_session(user.id)
+    """session = db.get_session(user.id)
     if session.user1_id == user.id:
         user_status = session.user1_status
         opponent = db.get_user(session.user2_id)
     else:
         user_status = session.user2_status
-        opponent = db.get_user(session.user1_id)
+        opponent = db.get_user(session.user1_id)"""
 
     gesture_name, gesture_image = get_gesture_screen_info(gesture)
-    user_status_text = get_user_status_screen_info(user_status)
-    emit('response', {"opponent": opponent.username, "status": user_status_text, "gesture_image": gesture_image, "gesture_name": gesture_name, "id_status": "Correct"})
+    user_status_text = get_user_status_screen_info(0)
+    emit('response', {"opponent": "Bot1", "status": user_status_text, "gesture_image": gesture_image, "gesture_name": gesture_name, "id_status": "Correct"})
 
 if __name__ == '__main__':
     #serve(app, host="0.0.0.0", port=5000)
