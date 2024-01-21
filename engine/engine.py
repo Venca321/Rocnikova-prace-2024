370c55623573c1b4d01b1eb4c038dce44ca65034
Venca321 <login@vaclavparma.cz>
2024-01-21 12:48:18 +0100

    Add READY and PLAYING status

--- engine/engine.py
+++ engine/engine.py
@@ -18,5 +18,15 @@ class GameEngine:
                 opponent = db.get_user(session.user1_id)
             except Exception:
                 return session, user_status, User("None", "?????", 0)
+            
+        if user_status == UserStatusEnums.WAITING and user.gesture == GestureEnums.LIKE:
+            user_status = UserStatusEnums.READY
+            db.update_session(user.id, user_status)
+
+        if user_status == UserStatusEnums.READY and opponent_status == UserStatusEnums.READY:
+            user_status = UserStatusEnums.PLAYING
+            opponent_status = UserStatusEnums.PLAYING
+            db.update_session(user.id, user_status)
+            db.update_session(opponent.id, opponent_status)
 
         return session, user_status, opponent
