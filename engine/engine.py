0dbbf277c52752932059340d6d5acea391c91554
Venca321 <login@vaclavparma.cz>
2024-01-21 11:55:19 +0100

    This is working, right? Right?

--- engine/engine.py
+++ engine/engine.py
@@ -4,4 +4,11 @@ from engine.database import Database, UserStatusEnums, User, Session
 
 class GameEngine:
     def process(session:Session, user:User, opponent:User) -> (Session, int, User):
-        pass
+        if user.id == session.user1_id:
+            user_status = session.user1_status
+            opponent_status = session.user2_status
+        else:
+            user_status = session.user2_status
+            opponent_status = session.user1_status
+
+        return session, user_status, opponent
