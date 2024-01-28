51e59ecf00b57d301b3dd89e8df0509800261a94
Venca321 <login@vaclavparma.cz>
2024-01-28 21:36:30 +0100

    Fix friend connection test

--- static/connect-friend-game/script.js
+++ static/connect-friend-game/script.js
@@ -1,4 +1,4 @@
 function connect_friend_game(){
-    const session_id = document.getElementById("session_id").valueOf(); 
+    const session_id = document.getElementById("session_id").value; 
     console.log(session_id)
 }
