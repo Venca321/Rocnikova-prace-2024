f5aabd4b5f1cd3be4a3c60863e1a7bbaffb0bc13
Venca321 <login@vaclavparma.cz>
2024-01-28 21:41:05 +0100

    Friend connection test 2

--- static/connect-friend-game/script.js
+++ static/connect-friend-game/script.js
@@ -1,4 +1,11 @@
-function connect_friend_game(){
+async function connect_friend_game(){
     const session_id = document.getElementById("session_id").value; 
-    console.log(session_id)
+
+    
+
+    await fetch("https://" + document.domain + ':' + location.port + "/connect-friend-game", {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({session_id: session_id})
+    });
 }
