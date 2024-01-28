ac102ad36db1b29e69afbd79091c036e951dfdd0
Venca321 <login@vaclavparma.cz>
2024-01-28 21:53:14 +0100

    Add create session test 1

---
+++ static/create-friend-game/script.js
@@ -0,0 +1,24 @@
+async function connect_friend_game(){
+    const urlParams = new URLSearchParams(window.location.search);
+    const name = urlParams.get('name');
+
+    response = await fetch("https://" + document.domain + ':' + location.port + "/register", {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({username: name})
+    });
+    data = await response.json();
+    const {user_id} = data;
+
+    response = await fetch("https://" + document.domain + ':' + location.port + "/create-friend-game", {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({user_id: user_id})
+    });
+    data = await response.json();
+    const {session_id} = data;
+
+    document.getElementById('session_id').innerText = session_id;
+
+    location.href = `?name=${name}&user_id=${user_id}`;
+}
