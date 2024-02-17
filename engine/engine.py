f7cd44fb883848736683cb3949d330090ba27d68
Venca321 <login@vaclavparma.cz>
2024-02-17 20:07:21 +0100

    Refactor

--- engine/engine.py
+++ engine/engine.py
@@ -5,22 +5,22 @@ import random
 
 
 class GameEngine:
-    def __evaluate_gestures(self, user_gesture:int, bot_gesture:int) -> int:
+    def __evaluate_gestures(self, gesture0:int, gesture1:int) -> int:
         """
         Evaluate gestures and return user status
         """
-        if user_gesture == bot_gesture:
+        if gesture0 == gesture1:
             return UserStatusEnums.TIED, UserStatusEnums.TIED
 
         valid_gestures = [GestureEnums.ROCK, GestureEnums.PAPER, GestureEnums.SCISSORS]
-        if user_gesture not in valid_gestures:
+        if gesture0 not in valid_gestures:
             return UserStatusEnums.LOSER
 
-        if user_gesture == GestureEnums.ROCK and bot_gesture == GestureEnums.SCISSORS:
+        if gesture0 == GestureEnums.ROCK and gesture1 == GestureEnums.SCISSORS:
             return UserStatusEnums.WINNER
-        elif user_gesture == GestureEnums.PAPER and bot_gesture == GestureEnums.ROCK:
+        elif gesture0 == GestureEnums.PAPER and gesture1 == GestureEnums.ROCK:
             return UserStatusEnums.WINNER
-        elif user_gesture == GestureEnums.SCISSORS and bot_gesture == GestureEnums.PAPER:
+        elif gesture0 == GestureEnums.SCISSORS and gesture1 == GestureEnums.PAPER:
             return UserStatusEnums.WINNER
         
         return UserStatusEnums.LOSER
@@ -29,14 +29,16 @@ class GameEngine:
         """
         Evaluate gesture and return status (win stay, lose change strategy)
         """
-        valid_gestures = [GestureEnums.ROCK, GestureEnums.PAPER, GestureEnums.SCISSORS]
         # sourcery skip: merge-nested-ifs
         if history:
-            if history[-1]["user_status"] == UserStatusEnums.LOSER and random.randint(0, 10) <= 8:
+            if (
+                history[-1]["user_status"] == UserStatusEnums.LOSER 
+                and random.randint(0, 10) <= 8
+            ):
                 bot_gesture = history[-1]["bot_gesture"]
                 return self.__evaluate_gestures(gesture, bot_gesture), bot_gesture
-            valid_gestures.remove(history[-1]["bot_gesture"])
 
+        valid_gestures = [GestureEnums.ROCK, GestureEnums.PAPER, GestureEnums.SCISSORS]
         bot_gesture = random.choice(valid_gestures)
         return self.__evaluate_gestures(gesture, bot_gesture), bot_gesture
 
